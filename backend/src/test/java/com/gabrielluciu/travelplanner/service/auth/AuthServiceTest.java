a78e01eb953426453d25597c27f338d7690ef095
Gabriel Luciu <gabrielluciu2013@gmail.com>
2026-02-28 09:51:48 +0200

    unit tests

---
+++ backend/src/test/java/com/gabrielluciu/travelplanner/service/auth/AuthServiceTest.java
@@ -0,0 +1,143 @@
+package com.gabrielluciu.travelplanner.service.auth;
+
+import com.gabrielluciu.travelplanner.dto.auth.AuthResponseDto;
+import com.gabrielluciu.travelplanner.dto.auth.LoginRequestDto;
+import com.gabrielluciu.travelplanner.dto.auth.RegisterRequestDto;
+import com.gabrielluciu.travelplanner.entity.auth.UserEntity;
+import com.gabrielluciu.travelplanner.exception.EmailAlreadyInUseException;
+import com.gabrielluciu.travelplanner.repository.user.UserRepository;
+import com.gabrielluciu.travelplanner.security.CustomUserDetails;
+import com.gabrielluciu.travelplanner.security.JwtService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtService jwtService;
+
+    @InjectMocks
+    private AuthService authService;
+
+
+    @Test
+    void register_success() {
+        // Arrange
+        RegisterRequestDto requestDto = new RegisterRequestDto(
+                "test@email.com",
+                "FirstName",
+                "LastName",
+                "password"
+        );
+
+        UUID userIdMock = UUID.randomUUID();
+        String jwtMock = "jwtMock";
+        String encodedPassword = "encodedPassword";
+        when(userRepository.existsByEmail(requestDto.email())).thenReturn(false);
+        when(passwordEncoder.encode(requestDto.password())).thenReturn(encodedPassword);
+
+        // set the id on the entity
+        when(userRepository.save(any(UserEntity.class))).thenAnswer(invocation -> {
+            UserEntity u = invocation.getArgument(0);
+            u.setId(userIdMock);
+            return u;
+        });
+
+        when(jwtService.generateToken(eq(userIdMock), any(), anyMap())).thenReturn(jwtMock);
+
+        // Act
+        AuthResponseDto responseDto = this.authService.register(requestDto);
+
+        // Assert
+        assertNotNull(responseDto);
+        assertEquals(userIdMock, responseDto.id());
+        assertEquals(requestDto.email(), responseDto.email());
+        assertEquals(requestDto.firstName(), responseDto.firstName());
+        assertEquals(requestDto.lastName(), responseDto.lastName());
+        assertEquals(jwtMock, responseDto.token());
+
+        verify(userRepository).save(any(UserEntity.class));
+        verify(passwordEncoder).encode(requestDto.password());
+    }
+
+    @Test
+    void register_emailAlreadyExists_throwsException() {
+        // Arrange
+        RegisterRequestDto requestDto = new RegisterRequestDto(
+                "test@email.com",
+                "FirstName",
+                "LastName",
+                "password"
+        );
+
+        when(userRepository.existsByEmail(requestDto.email())).thenReturn(true);
+
+        // Act & Assert
+        EmailAlreadyInUseException exception = assertThrows(EmailAlreadyInUseException.class, () -> authService.register(requestDto));
+
+        assertTrue(exception.getMessage().contains(requestDto.email()));
+
+        verify(userRepository, never()).save(any(UserEntity.class));
+        verify(passwordEncoder, never()).encode(any());
+        verifyNoInteractions(jwtService);
+    }
+
+    @Test
+    void login_success() {
+        // Arrange
+        LoginRequestDto requestDto = new LoginRequestDto("test@email.com", "password");
+        UUID userIdMock = UUID.randomUUID();
+        String jwtMock = "jwtMock";
+
+        CustomUserDetails userDetails = mock(CustomUserDetails.class);
+        when(userDetails.getId()).thenReturn(userIdMock);
+        when(userDetails.getEmail()).thenReturn("test@email.com");
+        when(userDetails.getFirstName()).thenReturn("FirstName");
+        when(userDetails.getLastName()).thenReturn("LastName");
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(jwtService.generateToken(eq(userIdMock), any(), any())).thenReturn(jwtMock);
+
+        // Act
+        AuthResponseDto responseDto = this.authService.login(requestDto);
+
+        // Assert
+        assertNotNull(responseDto);
+        assertEquals(userIdMock, responseDto.id());
+        assertEquals(requestDto.email(), responseDto.email());
+        assertEquals("FirstName", responseDto.firstName());
+        assertEquals("LastName", responseDto.lastName());
+        assertEquals(jwtMock, responseDto.token());
+
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtService).generateToken(eq(userIdMock), any(), anyMap());
+    }
+
+}
